ca81a5f47e2be106504832181fa983e2723dbc43
Nicolas Perriault <nperriault@gmail.com>
2015-05-21 10:03:33 +0200

    Added travis-ci configuration file.

--- test/index_test.js
+++ test/index_test.js
@@ -2,6 +2,12 @@ import { expect } from "chai";
 import { Cliquet, Collection } from "../src";
 
 describe("Cliquet", function() {
+  describe("#constructor()", function() {
+    it("should", function() {
+      // body...
+    });
+  });
+
   describe("#collection()", function() {
     it("should retrieve the collection object", function() {
       expect(new Cliquet("foo").collection("bar")).to.be.an.instanceOf(Collection);
