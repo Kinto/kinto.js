31ac8b5057c5947eef9fdda1d623200fc0962bc4
Mathieu Leplatre <mathieu@mozilla.com>
2015-06-19 14:05:43 +0200

    Add bucket option to Cliquetis

--- demo.js
+++ demo.js
@@ -1,5 +1,5 @@
 function main() {
-  var db = new Cliquetis({remote: "https://kinto.dev.mozaws.net/v0"});
+  var db = new Cliquetis({remote: "https://kinto.dev.mozaws.net/v1"});
   var tasks = db.collection("tasks");
 
   document.getElementById("form")
