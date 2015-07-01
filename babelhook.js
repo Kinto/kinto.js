3d784ab725ca08dcc3f9b07bada6b47e868ae74e
Nicolas Perriault <nperriault@gmail.com>
2015-07-02 01:23:45 +0200

    Fixed tests, added .babelrc

--- babelhook.js
+++
@@ -1,9 +0,0 @@
-/**
- * This file is required in mocha.opts
- * The only purpose of this file is to ensure
- * the babel transpiler is activated prior to any
- * test code, and using the same babel options
- */
-require("babel/register")({
-  stage: 1 // enables async functions, amongst other niceties
-});
