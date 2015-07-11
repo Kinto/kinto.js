de65cc9ecdc0007f0efa11f7c3cbec8b9d33a94d
Nicolas Perriault <nperriault@gmail.com>
2015-07-11 09:16:12 +0200

    Spawned request() in its own http module.

--- src/api.js
+++ src/api.js
@@ -2,13 +2,10 @@
 
 import { quote, unquote } from "./utils.js";
 import ERROR_CODES from "./errors.js";
+import request from "./http.js";
 
 const RECORD_FIELDS_TO_CLEAN = ["_status", "last_modified"];
 export const SUPPORTED_PROTOCOL_VERSION = "v1";
-export const DEFAULT_REQUEST_HEADERS = {
-  "Accept":       "application/json",
-  "Content-Type": "application/json",
-};
 
 export function cleanRecord(record, excludeFields=RECORD_FIELDS_TO_CLEAN) {
   return Object.keys(record).reduce((acc, key) => {
@@ -17,58 +14,6 @@ export function cleanRecord(record, excludeFields=RECORD_FIELDS_TO_CLEAN) {
     return acc;
   }, {});
 };
-
-/**
- * Performs an HTTP request to the Kinto server. Resolves with an objet
- * containing the following properties:
- *
- * - {Number}  status  The HTTP status code.
- * - {Object}  json    The JSON response body.
- * - {Headers} headers The response headers object (see ES6 fetch() spec).
- *
- * @param  {String} url
- * @param  {Object} options
- * @return {Promise}
- */
-export function request(url, options={headers:{}}) {
-  var response, status, statusText, headers;
-  // Ensure default request headers are always set
-  options.headers = Object.assign({}, DEFAULT_REQUEST_HEADERS, options.headers);
-  return fetch(url, options)
-    .then(res => {
-      response = res;
-      headers = res.headers;
-      status = res.status;
-      statusText = res.statusText;
-      if (headers.get("Content-Length") == 0) // 0 or "0"
-        return null;
-      return res.json();
-    })
-    .catch(err => {
-      const error = new Error(`HTTP ${status || 0}; ${err}`);
-      error.response = response;
-      error.stack = err.stack;
-      throw error;
-    })
-    .then(json => {
-      if (status >= 400) {
-        var message = `HTTP ${status}; `;
-        if (json.errno && json.errno in ERROR_CODES) {
-          message += ERROR_CODES[json.errno];
-          if (json.message) {
-            message += `: ${json.message}`;
-          }
-        } else {
-          message += statusText || "";
-        }
-        const error = new Error(message.trim());
-        error.response = response;
-        error.data = json;
-        throw error;
-      }
-      return {status, json, headers};
-    });
-}
 
 export default class Api {
   constructor(remote, options={headers: {}}) {
--- test/api_test.js
+++ test/api_test.js
@@ -4,12 +4,10 @@ import chai, { expect } from "chai";
 import chaiAsPromised from "chai-as-promised";
 import sinon from "sinon";
 import { quote } from "../src/utils";
-import Api, {
-  SUPPORTED_PROTOCOL_VERSION as SPV,
-  DEFAULT_REQUEST_HEADERS as DRH,
-  request,
-  cleanRecord
-} from "../src/api";
+import { fakeServerResponse } from "./test_utils.js";
+import Api, { SUPPORTED_PROTOCOL_VERSION as SPV, cleanRecord } from "../src/api";
+import { DEFAULT_REQUEST_HEADERS as DRH } from "../src/http.js";
+
 
 chai.use(chaiAsPromised);
 chai.should();
@@ -28,128 +26,6 @@ describe("Api", () => {
 
   afterEach(() => {
     sandbox.restore();
-  });
-
-  function fakeServerResponse(status, json, headers={}) {
-    return Promise.resolve({
-      status: status,
-      headers: {
-        get(name) {
-          return headers[name];
-        }
-      },
-      json() {
-        return json;
-      }
-    });
-  }
-
-  describe("request()", () => {
-    describe("Request headers", () => {
-      it("should set default headers", () => {
-        sandbox.stub(root, "fetch").returns(
-          fakeServerResponse(200, {}, {}));
-
-        request("/");
-
-        expect(fetch.firstCall.args[1].headers).eql(DRH);
-      });
-
-      it("should merge custom headers with default ones", () => {
-        sandbox.stub(root, "fetch").returns(
-          fakeServerResponse(200, {}, {}));
-
-        request("/", {headers: {Foo: "Bar"}});
-
-        expect(fetch.firstCall.args[1].headers.Foo).eql("Bar");
-      });
-    });
-
-    describe("Succesful request", () => {
-      beforeEach(() => {
-        sandbox.stub(root, "fetch").returns(
-          fakeServerResponse(200, {a: 1}, {b: 2}));
-      });
-
-      it("should resolve with HTTP status", () => {
-        return request("/")
-          .then(res => res.status)
-          .should.eventually.become(200);
-      });
-
-      it("should resolve with JSON body", () => {
-        return request("/")
-          .then(res => res.json)
-          .should.eventually.become({a: 1});
-      });
-
-      it("should resolve with headers", () => {
-        return request("/")
-          .then(res => res.headers.get("b"))
-          .should.eventually.become(2);
-      });
-    });
-
-    describe("No content response", () => {
-      it("should resolve with null JSON if Content-Length is 0", () => {
-        sandbox.stub(root, "fetch").returns(
-          fakeServerResponse(200, "", {"Content-Length": 0}));
-
-        return request("/")
-          .then(res => res.json)
-          .should.eventually.become(null);
-      });
-
-      it("should resolve with null JSON if Content-Length is '0'", () => {
-        sandbox.stub(root, "fetch").returns(
-          fakeServerResponse(200, "", {"Content-Length": "0"}));
-
-        return request("/")
-          .then(res => res.json)
-          .should.eventually.become(null);
-      });
-    });
-
-    describe("Malformed JSON response", () => {
-      it("should reject with an appropriate message", () => {
-        sandbox.stub(root, "fetch").returns(Promise.resolve({
-          status: 200,
-          headers: {
-            get(name) {
-              return "fake";
-            }
-          },
-          json() {
-            return JSON.parse("malformed json");
-          }
-        }));
-
-        return request("/")
-          .should.be.rejectedWith(Error, /HTTP 200; SyntaxError: Unexpected token/);
-      });
-    });
-
-    describe("Business error responses", () => {
-      it("should reject on status code > 400", () => {
-        sandbox.stub(root, "fetch").returns(
-          fakeServerResponse(400, {
-            code: 400,
-            details: [
-              {
-                description: "data is missing",
-                location: "body",
-                name: "data"
-              }
-            ],
-            errno: 107,
-            error: "Invalid parameters",
-            message: "data is missing"
-        }));
-
-        return request("/")
-          .should.be.rejectedWith(Error, /HTTP 400; Invalid request parameter: data is missing/);
-      });
-    });
   });
 
   describe("#constructor", () => {
