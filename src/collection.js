87980d5a7684427ac27ad00d4727a14f2e1e01b8
Mathieu Leplatre <mathieu@leplat.re>
2016-05-10 10:39:42 +0200

    Avoid redownloading our own changes (fixes #144) (#424)

--- src/collection.js
+++ src/collection.js
@@ -727,10 +727,22 @@ export default class Collection {
       lastModified: this.lastModified,
       headers: {},
     }, options);
+
+    // Optionally ignore some records when pulling for changes.
+    // (avoid redownloading our own changes on last step of #sync())
+    let filters;
+    if (options.exclude) {
+      // Limit the list of excluded records to the first 50 records in order
+      // to remain under de-facto URL size limit (~2000 chars).
+      // http://stackoverflow.com/questions/417142/what-is-the-maximum-length-of-a-url-in-different-browsers/417184#417184
+      const exclude_id = options.exclude.slice(0, 50).map((r) => r.id).join(",");
+      filters = {exclude_id};
+    }
     // First fetch remote changes from the server
     return this._apiCollection.listRecords({
       since: options.lastModified || undefined,
-      headers: options.headers
+      headers: options.headers,
+      filters
     })
     .then(({data, last_modified}) => {
       // last_modified is the ETag header value (string).
@@ -745,7 +757,7 @@ export default class Collection {
       const localSynced = options.lastModified;
       const serverChanged = unquoted > options.lastModified;
       const emptyCollection = data.length === 0;
-      if (localSynced && serverChanged && emptyCollection) {
+      if (!options.exclude && localSynced && serverChanged && emptyCollection) {
         throw Error("Server has been flushed.");
       }
 
@@ -820,25 +832,29 @@ export default class Collection {
         const conflicts = synced.conflicts.map((c) => {
           return {type: c.type, local: c.local.data, remote: c.remote};
         });
+        // Merge outgoing conflicts into sync result object
+        syncResultObject.add("conflicts", conflicts);
+
+        // Reflect publication results locally using the response from
+        // the batch request.
+        // For created and updated records, the last_modified coming from server
+        // will be stored locally.
         const published = synced.published.map((c) => c.data);
         const skipped = synced.skipped.map((c) => c.data);
 
-        // Merge outgoing conflicts into sync result object
-        syncResultObject.add("conflicts", conflicts);
-        // Reflect publication results locally
+        // Records that must be deleted are either deletions that were pushed
+        // to server (published) or deleted records that were never pushed (skipped).
         const missingRemotely = skipped.map(r => Object.assign({}, r, {deleted: true}));
         const toApplyLocally = published.concat(missingRemotely);
-        // Deleted records are distributed accross local and missing records
-        // XXX: When tackling the issue to avoid downloading our own changes
-        // from the server. `toDeleteLocally` should be obtained from local db.
-        // See https://github.com/Kinto/kinto.js/issues/144
+
         const toDeleteLocally = toApplyLocally.filter((r) => r.deleted);
         const toUpdateLocally = toApplyLocally.filter((r) => !r.deleted);
+
         // First, apply the decode transformers, if any
         return Promise.all(toUpdateLocally.map(record => {
           return this._decodeRecord("remote", record);
         }))
-          // Process everything within a single transaction
+          // Process everything within a single transaction.
           .then((results) => {
             return this.db.execute((transaction) => {
               const updated = results.map((record) => {
@@ -970,7 +986,9 @@ export default class Collection {
         if (result.published.length === 0) {
           return result;
         }
-        return this.pullChanges(result, options);
+        // Avoid redownloading our own changes during the last pull.
+        const pullOpts = Object.assign({}, options, {exclude: result.published});
+        return this.pullChanges(result, pullOpts);
       });
     // Ensure API default remote is reverted if a custom one's been used
     return pFinally(syncPromise, () => this.api.remote = previousRemote);
