6a2ceb45a1834e2c3a26be59cb41cdb801138624
Mathieu Leplatre <mathieu@mozilla.com>
2017-01-16 16:32:00 +0100

    Add test for retry in the middle of sync

--- test/collection_test.js
+++ test/collection_test.js
@@ -2183,7 +2183,7 @@ describe("Collection", () => {
     it("should use a custom remote option", () => {
       sandbox.stub(articles, "importChanges");
       sandbox.stub(articles, "pushChanges").returns(new SyncResultObject());
-      const fetch = sandbox.stub(root, "fetch")
+      const fetch = sandbox.stub(global, "fetch")
         .returns(fakeServerResponse(200, {data: []}, {}));
 
       return articles.sync({remote: "http://test/v1"}).
@@ -2195,7 +2195,7 @@ describe("Collection", () => {
     it("should revert the custom remote option on success", () => {
       sandbox.stub(articles, "importChanges");
       sandbox.stub(articles, "pushChanges").returns(new SyncResultObject());
-      sandbox.stub(root, "fetch")
+      sandbox.stub(global, "fetch")
         .returns(fakeServerResponse(200, {data: []}, {}));
 
       return articles.sync({remote: "http://test/v1"}).
@@ -2207,7 +2207,7 @@ describe("Collection", () => {
     it("should revert the custom remote option on failure", () => {
       sandbox.stub(articles, "importChanges");
       sandbox.stub(articles, "pushChanges").returns(Promise.reject("boom"));
-      sandbox.stub(root, "fetch")
+      sandbox.stub(global, "fetch")
         .returns(fakeServerResponse(200, {data: []}, {}));
 
       return articles.sync({remote: "http://test/v1"}).
@@ -2358,6 +2358,38 @@ describe("Collection", () => {
 
         return articles.sync({ignoreBackoff: true})
           .then(_ => sinon.assert.calledOnce(pullChanges));
+      });
+    });
+
+    describe("Retry", () => {
+      let fetch;
+
+      beforeEach(() => {
+        // Disable stubbing of kinto-http, stub low-level fetch instead.
+        sandbox.restore();
+        fetch = sandbox.stub(global, "fetch");
+        sandbox.stub(global, "setTimeout", (fn) => setImmediate(fn));
+        // Settings
+        fetch.onCall(0).returns(fakeServerResponse(200, {settings:{}}, {}));
+        // Pull
+        fetch.onCall(1).returns(fakeServerResponse(200, {data: []}, {}));
+        // Push
+        fetch.onCall(2).returns(fakeServerResponse(503, {}, {"Retry-After": "1"}));
+        fetch.onCall(3).returns(fakeServerResponse(200, {responses: [
+          {status: 201, body: {data: {id: 1, last_modified: 41}}},
+          {status: 201, body: {data: {id: 2, last_modified: 42}}},
+          {status: 201, body: {data: {id: 3, last_modified: 43}}}
+        ]}, {"ETag": '"123"'}));
+        // Last pull
+        fetch.onCall(4).returns(fakeServerResponse(200, {data: []}, {}));
+      });
+
+      it("should retry if specified", () => {
+        return articles.sync({retry: 3})
+          .then((result) => {
+            //console.log(fetch.getCalls());
+            expect(result.ok).eql(true);
+          });
       });
     });
   });
