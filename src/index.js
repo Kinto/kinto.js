3f59358ed99e41716b53ce2ff859fd2637c7caae
Christian Rackerseder <git@echooff.de>
2017-02-13 07:00:39 +0100

    Removed browserify compatibility

--- src/index.js
+++ src/index.js
@@ -28,9 +28,3 @@ export default class Kinto extends KintoBase {
     super({...defaults, ...options});
   }
 }
-
-// This fixes compatibility with CommonJS required by browserify.
-// See http://stackoverflow.com/questions/33505992/babel-6-changes-how-it-exports-default/33683495#33683495
-if (typeof module === "object") {
-  module.exports = Kinto;
-}
