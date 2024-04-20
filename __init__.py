0cf004aca80df0089e78e25f571ab74eceb44d86
khanfareena <khansfareena@gmail.com>
2024-04-20 01:29:53 -0400

    Implement custom authentication policy in __init__.py

--- __init__.py
+++ __init__.py
@@ -1,2 +1,12 @@
-from pyramid.config import Configurator
-from .authentication import GroupAwareAuthenticationPolicy
+from pyramid.view import view_config
+from pyramid.security import remember, forget
+
+@view_config(route_name='login', request_method='POST')
+def login(request):
+    username = request.params['username']
+    password = request.params['password']
+
+    if check_credentials(username, password):
+        headers = remember(request, username)
+        return HTTPFound(location="/", headers=headers)
+    return HTTPUnauthorized()
