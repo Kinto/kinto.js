6449c181e5244188968ca3880dcfe9dcfe29eb06
khanfareena <khansfareena@gmail.com>
2024-04-23 00:29:02 -0400

    Update effective_principals to include user and group roles

--- src/service/authentication.py
+++ src/service/authentication.py
@@ -1,18 +1,29 @@
+import jwt
 from pyramid.authentication import CallbackAuthenticationPolicy
 from pyramid.security import Everyone, Authenticated
-import jwt  # PyJWT
 
 class GroupAwareAuthenticationPolicy(CallbackAuthenticationPolicy):
   def __init__(self, secret, *args, **kwargs):
     self.secret = secret
     super().__init__(*args, **kwargs)
 
-    def unauthenticated_userid(self, request):
-      token = request.headers.get('Authorization', '').split(None, 1)[-1]
-      try:
-        claims = jwt.decode(token, self.secret, algorithms=['HS256'])
-        request.jwt_claims = claims
-        return claims.get('sub')
-      except jwt.ExpiredSignatureError:
-        request.jwt_claims = {}
-        return None
+  def unauthenticated_userid(self, request):
+    token = request.headers.get('Authorization', '').split(None, 1)[-1]
+    try:
+      claims = jwt.decode(token, self.secret, algorithms=['HS256'])
+      request.jwt_claims = claims
+      return claims.get('sub')
+    except jwt.ExpiredSignatureError:
+      request.jwt_claims = {}
+      return None
+
+  def effective_principals(self, request):
+    principals = [Everyone]
+    userid = self.unauthenticated_userid(request)
+    if userid:
+      principals.append(Authenticated)
+      principals.append(f'user:{userid}')
+      groups = request.jwt_claims.get('groups', [])
+      for group in groups:
+        principals.append(f'group:{group}')
+    return principals
